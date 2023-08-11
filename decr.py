977ce921777640cf085dc9dd3ede3509a02de3cc
hanahye <ithea@qq.com>
2023-08-11 23:21:40 +0800

    init

---
+++ decr.py
@@ -0,0 +1,33 @@
+import os
+
+total_bundles = 0
+success_count = 0
+error_count = 0
+
+
+def decrypt_and_save(file_path):
+    global total_bundles, success_count, error_count
+    total_bundles += 1
+    print(file_path)
+    with open(file_path, "rb") as f:
+        data = f.read()
+    result = bytearray(data)[50:]
+    key = result[0] ^ 0x55  # U
+    if key != result[1] ^ 0x6E:  # n
+        print("Decryption Error: Not an Unity File!")
+        error_count += 1
+    else:
+        result = [byte ^ key for byte in result]
+        decrypted_file_path = file_path + ".decr"
+        with open(decrypted_file_path, "wb") as f:
+            f.write(bytes(result))
+        success_count += 1
+
+
+if __name__ == "__main__":
+    for root, dirs, files in os.walk(os.getcwd()):
+        for file in files:
+            if file.endswith(".bundle"):
+                decrypt_and_save(os.path.join(root, file))
+    print(f"\nDecryption completed. {total_bundles} in total, {success_count} successful, {error_count} failed.")
+    input("Press any key to exit ...")
